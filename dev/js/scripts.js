df3687599741618663775d35882593136c714679
EricSweeney <ejs363@drexel.edu>
2022-04-18 18:42:13 -0400

    Update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -38,13 +38,15 @@ import { gsap } from "gsap";
 //Labels
 // "someLabelName"
 
+//const mainTL = gsap.timeline();
+
+//mainTL
+//.from("#hero article", {duration:2, yPercent:50})
+//.from("#hero h2", {duration:0.5, y:200, rotation:180, color:"#ec6602", alpha:0}, "herotext")
+//.from("#hero h1", {duration:0.5, y:"+=200", rotation:180, alpha:0}, "herotext")
+
 const mainTL = gsap.timeline();
-
-mainTL
-.from("#hero article", {duration:2, yPercent:50})
-.from("#hero h2", {duration:0.5, y:200, rotation:180, color:"#ec6602", alpha:0}, "herotext")
-.from("#hero h1", {duration:0.5, y:"+=200", rotation:180, alpha:0}, "herotext")
-
-
+ 
+mainTL.to(".hero-content", {duration: 2, x:500})
 
 ;
